5e582436f7ee6fd037487de9a66c3c46ebc0c4f2
Jeffrey Biles <bilesjeffrey@gmail.com>
2022-12-01 17:00:17 -0600

    day 1, part 1

---
+++ day-01/p1.ts
@@ -0,0 +1,43 @@
+type Elf = number[]
+
+const firstExample: Elf[] = [
+  [
+    1000,
+    2000,
+    3000,
+  ],
+  [
+    4000,
+  ],
+  [
+    5000,
+    6000,
+  ],
+  [
+    7000,
+    8000,
+    9000,
+  ],
+  [
+    10000,
+  ],
+]
+
+const getCaloriesPerElf = (elves: Elf[]): Elf => {
+  return elves.map((elf: number[]) => {
+    return elf.reduce((partial, num) => partial + num, 0)
+  })
+}
+
+const getMaxCalories = (elves: Elf[]): number => {
+  return Math.max(...getCaloriesPerElf(elves))
+}
+
+const getElfData = async (textFilePath: string): Promise<Elf[]> => {
+  const data = await Deno.readTextFile(textFilePath)
+  const elves = data.split('\n\n')
+  return elves.map(elf => elf.split('\n').map(line => parseInt(line)))
+}
+
+const elfData = await getElfData('./day-01/data.txt')
+console.log(getMaxCalories(elfData))
